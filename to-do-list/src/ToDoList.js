9cbb27728842602e8d07a39f7bde071192ca1b92
Amari James <mar.james1998@gmail.com>
2020-12-08 16:07:19 -0500

    listlab

---
+++ to-do-list/src/ToDoList.js
@@ -0,0 +1,14 @@
+function ToDoList() {
+    return (
+        <div>
+            <h1>My List</h1>
+            <ul>
+                <li>Learn React</li>
+                <li>????</li>
+                <li>Profit</li>
+            </ul>
+        </div>
+    );
+}
+
+export default ToDoList;
